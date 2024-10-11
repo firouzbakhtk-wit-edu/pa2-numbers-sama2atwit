41896e94e445cf141a0602db335d4a52777e0db3
Alex Sam <sama2@wit.edu>
2024-10-10 22:11:58 -0400

    Submission

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,135 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
-// TODO: document this class
+/**
+ * Solution to PA2a
+ * When it runs, the program will ask the user for 5 whole numbers.
+ * It will take those 5 numbers and calculate the sum and average of
+ * all, positive, and non-positive numbers
+ * 
+ * @author Alex Sam
+ */
 public class PA2a {
 
-	// TODO: document this function
+	/**
+	 * Starts the program, asks for 5 whole numbers (no decimals)
+	 * Start calculating the amount of positive and non-positive integers
+	 * their sum, and their average along with the sum and average of both
+	 * 
+	 * @param args command-line arguments, ignored
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		// Scanner object initialization
+		Scanner input = new Scanner(System.in);
+		
+		// Ask the user and assign the values to 5 variables
+		System.out.printf("Enter five whole numbers: ");
+		int a = input.nextInt();
+		int b = input.nextInt();
+		int c = input.nextInt();
+		int d = input.nextInt();
+		int e = input.nextInt();
+		
+		// Initialize variables for the sums and amount of positive/non-positive numbers
+		int pos_num = 0;
+		int non_num = 0;
+		
+		int pos_sum = 0;
+		int non_sum = 0;
+		
+		// Checks to see if the first input is a positive number or not (greater than 0)
+		// Also increase pos_num or non_num by 1 when it finds a positive or non-positive number
+		// And gets added to the sum of pos_sum or non_sum
+		// The same process is applied to the other inputs/variables
+		if(a > 0) {
+			pos_num++;
+			pos_sum = pos_sum + a;
+		}
+		else {
+			non_num++;
+			non_sum = non_sum + a;
+		}
+		
+		if(b > 0) {
+			pos_num++;
+			pos_sum = pos_sum + b;
+		}
+		else {
+			non_num++;
+			non_sum = non_sum + b;
+		}
+		
+		if(c > 0) {
+			pos_num++;
+			pos_sum = pos_sum + c;
+		}
+		else {
+			non_num++;
+			non_sum = non_sum + c;
+		}
+		
+		if(d > 0) {
+			pos_num++;
+			pos_sum = pos_sum + d;
+		}
+		else {
+			non_num++;
+			non_sum = non_sum + d;
+		}
+		
+		if(e > 0) {
+			pos_num++;
+			pos_sum = pos_sum + e;
+		}
+		else {
+			non_num++;
+			non_sum = non_sum + e;
+		}
+		
+		// Complete the sum of all whole numbers and calculate the average of positive/non-positive/whole numbers
+		// Now we will have the pos_sum, non_sum, sum, pos_average, non_average, and average
+		int sum = pos_sum + non_sum;
+		
+		double pos_average;
+		double non_average;
+		
+		// pos_plural and non_plural account for the case where there are exactly one positive or non-positive number
+		// (defaults to "numbers" in case there are more than 1 or none)
+		String pos_plural = "numbers";
+		String non_plural = "numbers";
+		
+		if(pos_num == 0) {
+			pos_average = 0.00; // when there are 0 positive numbers, the average will be 0.00 (and not NaN)
+		}
+		else {
+			if(pos_num == 1) {
+				pos_plural = "number"; // when there are exactly 1 positive numbers
+			}
+			pos_average = 1.0 * pos_sum / pos_num; // double value before the division to convert pos_sum into a double
+		}
+		
+		if(non_num == 0) {
+			non_average = 0.00;
+		}
+		else {
+			if(non_num == 1) {
+				non_plural = "number";
+			}
+			non_average = 1.0 * non_sum / non_num;
+		}
+		
+		double average = 1.0 * sum / 5;
+		
+		// Print out the results
+		System.out.printf("The sum of the %d positive " + pos_plural + ": %d%n", pos_num, pos_sum);
+		System.out.printf("The sum of the %d non-positive " + non_plural + ": %d%n", non_num, non_sum);
+		System.out.printf("The sum of the 5 numbers: %d%n", sum);
+		System.out.printf("The average of the %d positive " + pos_plural + ": %.2f%n", pos_num, pos_average);
+		System.out.printf("The average of the %d non-positive " + non_plural + ": %.2f%n", non_num, non_average);
+		System.out.printf("The average of the 5 numbers: %.2f%n", average);
+		
+		// Close input
+		input.close();
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,53 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
-// TODO: document this class
+/**
+ * Solution to PA2b
+ * When it runs, the program will ask the user for double variables a, b, and c in that order.
+ * It will then calculate the discriminant of b^2 - 4ac
+ * which will discover how many roots are there that solves the quadratic equation ax^2 + bx + c = 0.
+ * 
+ * @author Alex Sam
+ */
 public class PA2b {
 
-	// TODO: document this function
+	/**
+	 * Ask the user for 3 inputs: a, then b, then c,
+	 * calculate the discriminant, then print out the root(s) or x
+	 * to get 0 in ax^2 + bx + c
+	 * 
+	 * @param args command-line arguments, ignored
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		// Scanner initialization
+		Scanner input = new Scanner(System.in);
+		
+		// Ask for a, b, and c
+		System.out.printf("Enter a b c: ");
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+		
+		// Calculate discriminant
+		// b^2 - 4ac
+		double discriminant = b*b - (4 * a * c);
+		
+		// if discriminant is negative, the roots will be imaginary numbers
+		if(discriminant < 0) {
+			System.out.printf("Roots: imaginary%n");
+		}
+		// if discriminant is greater than 0, there will be 2 roots
+		// (-b +/- [the square root of the discriminant]) / 2a
+		else if(discriminant > 0) {
+			System.out.printf("Roots: %.2f, %.2f%n", (-b - Math.sqrt(discriminant)) / (2*a), (-b + Math.sqrt(discriminant)) / (2*a));
+		}
+		// if discriminant is 0, there will be 1 root as the square root of 0 is 0
+		// -b / 2a
+		else {
+			System.out.printf("Root: %.2f%n", -b / (2*a));
+		}
+		
+		input.close();
 	}
 
 }
